345ef7d4c02fff2d50126805a0b24f0e1d30e32a
Kurtis <kcd257@yahoo.com>
2022-04-13 21:01:49 -0700

    work on client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Test from "./components/Test";
+import Profile from "./components/Profile"
 function App() {
   return <Test />;
+  <Profile />;
 }
 
 export default App;
